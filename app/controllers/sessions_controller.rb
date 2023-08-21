c9a8a3e810f384415aa2167bbc3a7db6400e5de6
thanhtt-3322 <SUN-ASTERISK\tran.trung.thanh@b123322-pc.sun-asterisk.com>
2023-08-21 19:23:54 +0700

    register4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      flash[:success] = "Signed in successfully!"
-      sign_in user
-      redirect_to root_path
-    else
-      flash.now[:error] = "Email or password is invalid!"
-      render :new
-    end
+    # user = User.find_by(email: params[:email])
+    # if user && user.authenticate(params[:password])
+    #   flash[:success] = "Signed in successfully!"
+    #   sign_in user
+    #   redirect_to root_path
+    # else
+    #   flash.now[:error] = "Email or password is invalid!"
+    #   render :new
+    # end
   end
 
   def destroy
